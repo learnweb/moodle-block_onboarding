f66f3bc4543f55ce6fc59cbeba114881bb9fdd14
David Koesters <david.koesters@uni-muenster.de>
2021-01-03 19:11:30 +0100

    minor changes and added comments

--- edit_step.php
+++ edit_step.php
@@ -30,33 +30,56 @@ if(has_capability('block/steps:edit_steps', $context)){
   $PAGE->set_title(get_string('edit_step', 'block_steps'));
   $PAGE->set_heading(get_string('edit_step', 'block_steps'));
 
-  require_once('./classes/forms/step_form.php');
+    require_once('./classes/forms/step_form.php');
+    /*
+     * optional_param speichert die übergebene URL-Variable step_id in step_id
+     * (bspw. moodle/blocks/steps/edit_step.php?step_id=8)
+     * sofern keine URL-Variable übergeben wurde, wird -1 in step_id gespeichert
+     * --> im Anwendungskontext wird somit differenziert ob es sich bei dem refrenzierten
+     * Schritt um einen neuen Schritt handelt (keine URL-Variable) oder ein bestehender
+     * Schrtitt editiert wird (vorhandene URL-Variable bzw. vorhandene step_id)
+     */
+    $step_id = optional_param('step_id', -1, PARAM_INT);
+    // Hilfsvaribale um Daten eines bestehenden Schritts zu speichern
+    $pStep = new stdClass;
+    $pStep->id = -1;
+    // wenn ein bestehender Schritt editiert werden soll, lese den Datensatz aus der Datenbank
+    if ($step_id != -1) {
+        /*
+         * speichere den Datensatz aus der Tabelle block_steps_steps (Tabebellen-Prefix nicht notwendig)
+         * bei dem die id-Daten mit der Variable step-id übereinstimmen (Konditionen werden immer als Array angegeben)
+         * wobei alle Felder bzw. Spalten des Datensatzes selektiert werden sollen
+         * und werfe keine Exception wenn keine oder mehrere Datensätze auf Basis der Konditionen gefunden werden
+         */
+        $pStep = $DB->get_record('block_steps_steps', array('id' => $step_id), $fields = '*', $strictness = IGNORE_MISSING);
+    }
+    /*
+     * erstelle eine neue Moodle Form als eine Step Form und übergebe zusätzlich die Hilfsvariable pStep
+     * welche beim Editieren eines bestehenden Schrittes die Daten des Schrittes aus der Datenbank enthält
+     */
+    $mform = new step_form(null, array('step' => $pStep));
 
-  $step_id = optional_param('step_id', -1, PARAM_INT);
-  $pStep = new stdClass;
-  $pStep->id = -1;
-  if($step_id != -1){
-    $pStep = $DB->get_record('block_steps_steps', array('id'=>$step_id), $fields='*', $strictness=IGNORE_MISSING);
-  }
-  $mform = new step_form(null, array('step' => $pStep));
-
-  if ($mform->is_cancelled()) {
-  		redirect('admin.php');
-  } else if ($fromform = $mform->get_data()) {
+    // wenn der Cancel-Button geklickt wird, kehre zu admin.php zurück
+    if ($mform->is_cancelled()) {
+        redirect('admin.php');
+        /*
+         * andernfalls wenn der Save-Changes-Button geklickt wurde und die Daten validiert werden konnten,
+         * lade die Daten aus der form und speichere diese in der form fromform
+         */
+    } else if ($fromform = $mform->get_data()) {
+        // speichere alle Daten aus der Form ausgenommen der position in dem Objekt step
       $step = new stdClass();
       $step->name = $fromform->name;
       $step->description = $fromform->description;
 
       $step->timemodified = time();
 
-      // hier ist edit step
+      // wenn ein bestehender Schritt editiert wird, aktualisiere den Datensatz
       if($fromform->id != -1){
         $step->id = $fromform->id;
         $step->position = $fromform->position;
         $DB->update_record('block_steps_steps', $step, $bulk=false);
-
-
-      //hier neuer step
+        // andernfalls wird ein neuer Schritt bzw. Datensatz hinzugefügt, dessen position aus der Form übernommen wird// andernfalls wird ein neuer Schritt bzw. Datensatz hinzugefügt, dessen position aus der Form übernommen wird
       }else{
           $step->timecreated = time();
           $step->position = $fromform->position;
@@ -69,6 +92,7 @@ if(has_capability('block/steps:edit_steps', $context)){
   echo $OUTPUT->header();
   $mform->display();
   echo $OUTPUT->footer();
+// andernfalls konnten die Daten nicht validiert werden und es wird ein Fehlerhinweis angezeigt
 }else{
   $PAGE->set_title(get_string('error', 'block_steps'));
   $PAGE->set_heading(get_string('error', 'block_steps'));
