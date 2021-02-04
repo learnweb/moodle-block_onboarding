84cd8bd5b0b6a311f74e88adefe5e1f2db80b57f
David Koesters <david.koesters@uni-muenster.de>
2021-02-04 12:25:52 +0100

    minor CSS updates and fixed amd function loading

--- amd/src/steps_view.js
+++ amd/src/steps_view.js
@@ -2,15 +2,13 @@ define(['jquery', 'core/ajax', 'core/notification'], function ($, ajax, notifica
 
     // TODO: Randfälle behandeln, z.B. letzter Schritt in Liste usw.
     // TODO: clean code
-
-
+    
     var init = function () {
 
 
         var promises = ajax.call([{
             methodname: 'block_onboarding_init_step',
-            args: {
-            }
+            args: {}
         }
         ]);
         promises[0].done(function (response) {
@@ -22,61 +20,49 @@ define(['jquery', 'core/ajax', 'core/notification'], function ($, ajax, notifica
     };
 
 
-    var next_step = function () {
+    $('.done_btn').on('click', function () {
+        var promises = ajax.call([{
+            methodname: 'block_onboarding_next_step',
+            args: {}
+        }
+        ]);
+        promises[0].done(function (response) {
+            $('.step_description').text(response.description);
+            $('.step_title').text('Step #' + response.position + ': ' + response.name);
+            $('.progress_bar_value').text(response.progress + '%');
+            $('.progress_bar_fill').css('width', (response.progress + '%'));
+        }).fail(notification.exception);
+    })
 
-        $('.done_btn').on('click', function () {
-            var promises = ajax.call([{
-                methodname: 'block_onboarding_next_step',
-                args: {
-                }
-            }
-            ]);
-            promises[0].done(function (response) {
-                $('.step_description').text(response.description);
-                $('.step_title').text('Step #' + response.position + ': ' + response.name);
-                $('.progress_bar_value').text(response.progress + '%');
-                $('.progress_bar_fill').css('width', (response.progress + '%'));
-            }).fail(notification.exception);
-        })
-    };
 
-    var skip_step = function () {
+    $('.skip_btn').on('click', function () {
+        var promises = ajax.call([{
+            methodname: 'block_onboarding_skip_step',
+            args: {}
+        }
+        ]);
+        promises[0].done(function (response) {
+            $('.step_description').text(response.description);
+            $('.step_title').text('Step #' + response.position + ': ' + response.name);
+        }).fail(notification.exception);
+    })
 
-        $('.skip_btn').on('click', function () {
-            var promises = ajax.call([{
-                methodname: 'block_onboarding_skip_step',
-                args: {
-                }
-            }
-            ]);
-            promises[0].done(function (response) {
-                $('.step_description').text(response.description);
-                $('.step_title').text('Step #' + response.position + ': ' + response.name);
-            }).fail(notification.exception);
-        })
-    };
 
-    var back_step = function () {
-
-        $('.back_btn').on('click', function () {
-            var promises = ajax.call([{
-                methodname: 'block_onboarding_back_step',
-                args: {
-                }
-            }
-            ]);
-            promises[0].done(function (response) {
-                $('.step_description').text(response.description);
-                $('.step_title').text('Step #' + response.position + ': ' + response.name);
-            }).fail(notification.exception);
-        })
-    };
+    $('.back_btn').on('click', function () {
+        var promises = ajax.call([{
+            methodname: 'block_onboarding_back_step',
+            args: {}
+        }
+        ]);
+        promises[0].done(function (response) {
+            $('.step_description').text(response.description);
+            $('.step_title').text('Step #' + response.position + ': ' + response.name);
+        }).fail(notification.exception);
+    })
 
 
     return {
-        init: init,
-        next_step: next_step(),
-        skip_step: skip_step(),
-        back_step: back_step(),
+        init: init
     };
-});
+})
+;
--- wiki/overview.php
+++ wiki/overview.php
@@ -24,9 +24,7 @@ global $USER, $DB;
 
 $PAGE->set_context($context);
 $PAGE->set_url(new moodle_url('/blocks/onboarding/wiki/overview.php'));
-// Achtung: Es kann nur eine Funktion gecalled werden?? -> WARUM MÜSSEN NICHT ALLE FUNKTIONEN GECALLED WERDEN??
 $PAGE->requires->js_call_amd('block_onboarding/steps_view', 'init');
-$PAGE->requires->js_call_amd('block_onboarding/steps_view', 'next_step');
 $PAGE->set_title(get_string('overview', 'block_onboarding'));
 $PAGE->set_heading(get_string('overview', 'block_onboarding'));
 $PAGE->navbar->add(get_string('pluginname', 'block_onboarding'));
